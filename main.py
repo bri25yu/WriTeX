6c3ccd79cdc51691bed59d10c278aee2294da18d
Brian Yu <bri25yu@berkeley.edu>
2019-02-09 11:17:23 -0800

    added basic plan

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import cv2, numpy as np, pypylon
 
+
+def main():
+    """
+    Main method for
+    """
+    # Create and initialize LaTex image to code mappingss
+    # Open "notepad"
+    while True: 
+        # while the user is running this program
+
+        # user writes something
+        # 2-3 second delay for the user to finish what they're writing
+        # convert user writing into latex code
+        # give user to the view pdf
+
+
+        #user has option to resize things
+        pass
 
 
 if __name__ == '__main__':
-    pass
+    main()
