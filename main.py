684620009e7388bf721ded7ea497ca751b94af3a
Brian Yu <bri25yu@berkeley.edu>
2019-02-09 11:26:19 -0800

    testing git

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ def main():
     """
     Main method for
     """
-    # Create and initialize LaTex image to code mappingss
+    # Create and initialize LaTex image to code mappings
+    load_latex()
     # Open "notepad"
+    open_window()
     while True: 
         # while the user is running this program
 
         # user writes something
         # 2-3 second delay for the user to finish what they're writing
+        
         # convert user writing into latex code
+
         # give user to the view pdf
 
 
-        #user has option to resize things
+        # user has option to resize things, edit latex code, etc
+        # Good UI ^^
         pass
 
 
