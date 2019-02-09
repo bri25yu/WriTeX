17b31cee7ffc35af81afa09238c77b03a62a3280
Ashwin Rastogi <ashwinrastogi@berkeley.edu>
2019-02-09 15:41:00 -0800

    Added training

--- recognition.py
+++ recognition.py
@@ -39,7 +39,34 @@ class Recognition(object):
 
         #Initializing 2nd pooling layer
         pool_2 = tf.layers.max_pooling2d(inputs = layer_2, pool_size = [2, 2], strides = 2)
-        flatten_pool2 = tf.reshape(pool_2, [-1, pool_2.size])
 
+        #Intializing final dense layer
+        flatten_pool2 = tf.reshape(pool_2, [-1, pool_2.size])
+        dense_layer = tf.layers.dense(inputs = flatten_pool2, units = 1024, activation = tf.nn.relu)
+        rectify = tf.layers.dropout(inputs = dense_layer, rate = 0.4, training = mode == tf.estimator.ModeKeys.TRAIN)
+
+        #Initializing Logits layer
+        logits = tf.layers.dense(inputs = rectify, units = 36)
+        predictions = {
+            "classes": tf.argmax(input=logits, axis=1),
+            "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+        }
+
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+            train_op = optimizer.minimize(
+                loss=loss,
+                global_step=tf.train.get_global_step())
+            return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+        eval_metric_ops = {
+            "accuracy": tf.metrics.accuracy(
+                labels=labels, predictions=predictions["classes"])
+        }
+        return tf.estimator.EstimatorSpec(
+            mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
