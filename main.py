8eebebaf06009c7e1810fc3bcd84038e2d88bf6f
Brian Yu <bri25yu@berkeley.edu>
2019-02-09 11:31:55 -0800

    Added basic files for main parts of the program

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ def main():
     """
     Main method for
     """
+    
     # Create and initialize LaTex image to code mappings
     load_latex()
+
     # Open "notepad"
     open_window()
+
     while True: 
         # while the user is running this program
 
         # user writes something
         # 2-3 second delay for the user to finish what they're writing
-        
+        # Say user is writing a summation: 3 parts: sigma, top index, bottom index
+
         # convert user writing into latex code
+        # RCNN
+        
+        # display converted text and images
 
         # give user to the view pdf
 
