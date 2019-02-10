349cc7993bc686eb19992720f576cb230b02c518
Brian Yu <bri25yu@berkeley.edu>
2019-02-10 05:02:54 -0800

    Finished recognition v4..? and cleaned up main

--- main.py
+++ main.py
@@ -1,68 +1,14 @@
-import numpy as np, tensorflow as tf
-import glob, sys, os
-import cv2
-from recognition import *
+from recognition import Network
+import recognition, loadLatex, UI
 
 def main():
 
     # Create and initialize LaTeX image to code mappingss
     # Open "notepad"
     
-    train_dir, test_dir = 'C:\\Users\\bri25\\Documents\\Python\\data\\', None
-    # Get training and testing data.
-    features, labels, mode = [], [], 0
-
-    recognition = Recognition(features, labels, mode)
-    recognition.feed_data(process_data(train_dir, test_dir))
-
-    classifier = tf.estimator.Estimator(model_fn = recognition.create_cnn)
-    log = {"Probabilities" : "softmax result"}
-    logging_hook = tf.train.LoggingTensorHook(tensors = log, every_n_iter = 50)
-
-    print("Training input")
-    # Train network using data
-    train_input = tf.estimator.inputs.numpy_input_fn(
-        x = {"x" : recognition.get_training_data()[0]},
-        y = recognition.get_training_data()[1],
-        batch_size = 500,
-        num_epochs = None,
-        shuffle = True,
-        num_threads = 10
-    )
-
-    # Create test 
-    test_input = tf.estimator.inputs.numpy_input_fn(
-        x = {"x" : recognition.get_testing_data()[0]},
-        y = recognition.get_testing_data()[1],
-        num_epochs = 1,
-        shuffle = False,
-        num_threads = 10
-    )
-
-    print("Training network")
-    # Classify and evaluate weights
-    classifier.train(
-        input_fn = recognition.train_cnn,
-        steps = 1,
-        hooks = [logging_hook]
-    )
-
-    print("Evaluating...")
-    eval_input = tf.estimator.inputs.numpy_input_fn(
-        x = {"x" : recognition.get_testing_data()[0]},
-        y = recognition.get_testing_data()[1],
-        batch_size = 500,
-        num_epochs = 1,
-        shuffle = False
-    )
-
-    #Output evaluation results
-    evaluation = classifier.evaluate(input_fn = eval_input)
-    print(evaluation)
-
+    TRAIN_DIR, TEST_DIR = 'C:\\Users\\bri25\\Documents\\Python\\data\\', None
+    recognition.main(TRAIN_DIR, TEST_DIR)
     
-
-
     while False: 
         # while the user is running this program
 
@@ -74,38 +20,6 @@ def main():
         #user has option to resize things
         pass
 
-def process_data(train_dir, test_dir = None):
-    """
-    If the data set given only has one set of data, use 95% for training and the other 5% for testing/validation.
-    If the data set only has one set of data, training is AUTOMATICALLY the first 95% of the data
-    """
-    print("Begin process data")
-    TESTING_TO_TRAINING_RATIO = 0.95
-    symbol_names = os.listdir(train_dir)
-    training_data, training_labels, testing_data, testing_labels = [], [], [], []
-
-    if not test_dir:
-        # for symbol_name in symbol_names:
-        for i in range(2):
-            symbol_name = symbol_names[i]
-            print(symbol_name)
-            image_files = os.listdir(train_dir + symbol_name)
-            i = 0
-            while i < int(TESTING_TO_TRAINING_RATIO*len(image_files)):
-                training_data.append(np.array(cv2.imread(train_dir + symbol_name + '\\' + image_files[i])))
-                training_labels.append(symbol_name)
-                i+=1
-            while i < len(image_files):
-                testing_data.append(np.array(cv2.imread(train_dir + symbol_name + '\\' + image_files[i])))
-                testing_labels.append(symbol_name)
-                i+=1
-    else:
-        # Since this is a first iteration and we only have a singular set of data 
-        #   (i.e. no official test data), it will automatically default to separating the 
-        #   testing and training data as above. 
-        pass
-    print("Finish process data")
-    return ((training_data, training_labels), (testing_data, testing_labels))
         
 if __name__ == '__main__':
     main()
--- recognition.py
+++ recognition.py
@@ -1,115 +1,99 @@
-import tensorflow as tf, numpy as np
+import tensorflow as tf, numpy as np, cv2
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.optimizers import RMSprop
+import os
 
-class Recognition:
-    '''
-    Convolutional Neural Network (CNN) object wrapper class.
-    Creates and trains the convolutional neural network for image recognition
-    '''
+# Default training and testing data directories:
+TRAIN_DIR, TEST_DIR = 'C:\\Users\\bri25\\Documents\\Python\\data\\', None
 
-    def __init__ (self, features, labels, mode):
-        self.features = features
-        self.labels = labels
-        self.mode = mode
+class Network:
+    """
+    Wrapper class for the CNN
+    """
+    IMAGE_SIZE, TRAINING_TO_TESTING_RATIO = 45, 0.95
 
-    def feed_data(self, data):
+    def __init__(self, train_dir, test_dir=None):
         """
-        Input data must be a second-level nested tuple
-        Refer to process_data() in "main.py" for proper format
+        Enable "eager execution"
+        Set the training and testing data directories. 
         """
-        assert type(data) == tuple, "Data is not a tuple"
-        assert type(data[0]) == tuple, "Training data is not a tuple"
-        assert type(data[1]) == tuple, "Testing data is not a tuple"
+        # tf.enable_eager_execution()
+        self.train_dir = train_dir + ("\\" if train_dir[-1] == "\\" else "")
+        if test_dir:
+            self.test_dir = test_dir + ("\\" if test_dir[-1] == "\\" else "")
+        else: 
+            self.test_dir = test_dir
 
-        self.training_data, self.training_labels = data[0][0], data[0][1]
-        self.testing_data, self.testing_labels = data[1][0], data[1][1]
-        print("Data fed")
+    def process_data(self, batch_size = 500):
+        """
+        This function takes the directories specified in the constructor and turns them into tf.datasets
 
-    def get_training_data (self):
+        If the data set given only has one set of data, use 95% for training and the other 5% for testing/validation.
+        If the data set only has one set of data, training is AUTOMATICALLY the first 95% of the data
         """
-        Returns a tuple of (training_data, training_labels)
+        self.batch_size = batch_size
+        symbol_names = os.listdir(self.train_dir)
+        self.training_data, self.training_labels = [], []
+
+        if not self.test_dir:
+            # for symbol_name in symbol_names:
+            for i in range(2):
+                symbol_name = symbol_names[i]
+                image_files = os.listdir(self.train_dir + symbol_name)
+                for i in range(len(image_files)):
+                    self.training_data.append(np.array(cv2.imread(self.train_dir + symbol_name + '\\' + image_files[i])))
+                    self.training_labels.append(ord(symbol_name))
+            self.validation_data = None
+        else:
+            testing_data, testing_labels = [], []
+            for symbol_name in symbol_names:
+                image_files = os.listdir(self.test_dir + symbol_name)
+                for i in range(len(image_files)):
+                    testing_data.append(np.array(cv2.imread(self.test_dir + symbol_name + '\\' + image_files[i])))
+                    testing_labels.append(ord(symbol_name))
+            self.validation_data = tuple([testing_data, testing_labels])
+        print("Finish process data")
+
+    def get_layers(self):
         """
-        return (self.training_data, self.training_labels)
+        Set the layers of the CNN as "logits" and "predictions"
+        """
+        self.model.add(Conv2D(64, 5, input_shape = (self.IMAGE_SIZE, self.IMAGE_SIZE, 3), padding="same", activation='relu'))
+        self.model.add(MaxPooling2D(pool_size = 2, strides = 2))
+        
+        for i in range(1):
+            self.model.add(Conv2D(64, 5, padding="same", activation='relu'))
+            self.model.add(MaxPooling2D(pool_size = 2, strides = 2))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(128, activation = 'relu'))
+        self.model.add(Dropout(0.4))
+        self.model.add(Dense(1, activation='softmax'))
+
+    def create_model(self):
+        self.model = Sequential()
+        self.get_layers()
+        self.model.compile(optimizer=RMSprop(lr=0.01),
+              loss="binary_crossentropy",
+              metrics=["acc"])
     
-    def get_testing_data (self):
+    def train_model(self, epochs = 10, batch_size = 500):
         """
-        Returns a tuple of (testing_data, testing_labels)
+        Call the model training function. 
         """
-        return (self.testing_data, self.testing_labels)
+        if self.validation_data:
+            self.model.fit(np.array(self.training_data), np.array(self.training_labels), epochs=epochs, 
+                            batch_size=batch_size, validation_data=self.validation_data)
+        else:
+            self.model.fit(np.array(self.training_data), np.array(self.training_labels), epochs=epochs, 
+                            batch_size=batch_size, validation_split=self.TRAINING_TO_TESTING_RATIO)
 
-    def create_cnn (self, features, labels, mode):
-        print("Creating cnn\n\n\n\n\n\n\n\n\n\n\n\n")
-        self.labels, self.mode = labels, mode
-        # Initializes input layer
-        input_layer = tf.reshape(features["x"], [-1, 28, 28])
+def main(TRAIN_DIR, TEST_DIR):
+    my_network = Network(TRAIN_DIR, TEST_DIR)
+    my_network.create_model()
+    my_network.process_data()
+    my_network.train_model()
 
-        # Initializes first convolutional layer
-        layer_1 = tf.layers.conv2d(
-            inputs = input_layer, 
-            filters = 32,
-            kernel_size = [5, 5],
-            padding = "same",
-            activation = tf.nn.relu)
-        
-        # Initializes first max pooling layer
-        max_pool_1 = tf.layers.max_pooling2d(inputs = layer_1, pool_size = [2, 2], strides = 2)
-        
-        # Initializes second convolutional layer
-        layer_2 = tf.layers.conv2d(
-            inputs = max_pool_1,
-            filters = 64,
-            kernel_size = [5, 5],
-            padding = "same",
-            activation = tf.nn.relu)
-
-        # Initializes second max pooling layer
-        max_pool_2 = tf.layers.max_pooling2d(inputs = layer_2, pool_size = [2, 2], strides = 2)
-
-        # Initializes third convolutional layer
-        layer_3 = tf.layers.conv2d(
-            inputs = max_pool_2,
-            filters = 128,
-            kernel_size = [5, 5],
-            padding = "same",
-            activation = tf.nn.relu
-        )
-
-        # Initializes third max pooling layer
-        max_pool_3 = tf.layers.max_pooling2d(inputs = layer_3, pool_size = [2, 2], strides = 2)
-
-        # Projects onto dense layer
-        flatten_pool_3 = tf.reshape(max_pool_3, [-1, max_pool_3.size])
-        dense_layer = tf.layers.dense(inputs = flatten_pool_3, units = 1024, activation = tf.nn.relu)
-        rectify = tf.layers.dropout(inputs = dense_layer, rate = 0.4, training = mode == tf.estimator.ModeKeys.TRAIN)
-
-        # Creates logits and final output probability layer
-        self.logits = tf.layers.dense(inputs = rectify, units = 36)
-
-        self.predictions = {
-            "classes": tf.argmax(input = self.logits, axis = 1),
-            "probabilities": tf.nn.softmax(self.logits, name = "softmax_tensor")
-        }
-        
-    def predict(self):
-        # For predicting provided image
-
-        if self.mode == tf.estimator.ModeKeys.PREDICT:
-            return tf.estimator.EstimatorSpec(mode = self.mode, prediction = self.predictions)
-        eval_metric_ops = {
-            "Result accuracy" : tf.metrics.accuracy(labels = self.labels, 
-            predictions = self.predictions["classes"])
-            }
-
-        return tf.estimator.EstimatorSpec(mode=self.mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-    def train_cnn(self):
-        # Trains and back-propagates weights given data
-        self.training_data /= np.float32(255)
-        self.testing_data /= np.float32(255)
-        
-        # For training and back-propagating weights
-        if self.mode == tf.estimator.ModeKeys.TRAIN:
-            optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
-            train_op = optimizer.minimize(
-                loss = tf.losses.sparse_softmax_cross_entropy(labels=self.labels, logits=self.logits), #also try just =loss
-                global_step = tf.train.get_global_step())
-            return tf.estimator.EstimatorSpec(mode = self.mode, loss = loss, train_op = train_op)
+if __name__ == '__main__':
+    main(TRAIN_DIR, TEST_DIR)
