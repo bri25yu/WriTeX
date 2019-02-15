11d3f2343eabedbc45049495448baab817be0cf0
Brian Yu <bri25yu@berkeley.edu>
2019-02-14 23:08:12 -0800

    Hella cleaned up recognition.py, added a new custom library (at least the starts), and reworked some of the CNN logic

--- recognition.py
+++ recognition.py
@@ -1,137 +1,100 @@
-import tensorflow as tf, numpy as np, cv2
+""" Main file for running the CNN and RNN for handwriting recognition. """
+
+# TensorFlow and Keras imports
+import tensorflow as tf
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from tensorflow.python.keras.models import Sequential, save_model, load_model
 from tensorflow.python.keras.optimizers import *
-from tensorflow.python.keras.utils import to_categorical
+
+# Other library imports
+import numpy as np, cv2
 from multiprocessing.dummy import Pool as ThreadPool 
 import os
-import time
 
-# Default training and testing data directories:
-TRAIN_DIR, TEST_DIR = 'C:\\Users\\bri25\\Documents\\Python\\data\\', None
-SAVE_DIR = 'data/'
+# Custom library imports
+from recognition_lib import *
 
 class Network:
-    """
-    Wrapper class for the CNN
-    TRY: Normalize data to between [0, 1]
-    Implement Adam optimizer
-    """
-    IMAGE_SIZE, TRAINING_TO_TESTING_RATIO = 45, 0.95
-    NUM_THREADS = 100
+    """ Wrapper class for the CNN """
+    IMAGE_SIZE, TESTING_TO_TRAINING_RATIO = 45, 0.95
+    NUM_THREADS = 10
 
-    def __init__(self, train_dir, save_dir, test_dir=None):
+    def __init__(self, model_dir, **kwargs):
+        """ Set the training and testing data directories. 
+        @param model_dir: Directory to load and save the model
+        @param training_dir: Directory to get the training data from
+        @param testing_dir: Directory to get the testing data from
         """
-        Set the training and testing data directories. 
-        """
-        # tf.enable_eager_execution()
-        self.train_dir = train_dir + ("\\" if train_dir[-1] == "\\" else "")
-        self.save_dir = save_dir
-        if test_dir:
-            self.test_dir = test_dir + ("\\" if test_dir[-1] == "\\" else "")
-        else: 
-            self.test_dir = test_dir
+        self.model_dir = check_folder_style(model_dir)
+        self.training_dir = check_folder_style(kwargs['training_dir']) if 'training_dir' in kwargs else None
+        self.testing_dir = check_folder_style(kwargs['testing_dir']) if 'testing_dir' in kwargs else None
+        self.load_previous_model()
 
-    def append_data(self, data, labels, fname, name):
-        """
-        Method for how data is appended to later be passed into the network. 
-        """
-        data.append(np.array(cv2.imread(fname)))
-        labels.append(name)
+    def load_previous_model(self):
+        self.model = load_model(self.model_dir + 'model')
+
+    def create_model(self):
+        self.model = Sequential()
+        self.get_layers()
+        self.model.compile(
+                            optimizer='rmsprop',
+                            loss='categorical_crossentropy',
+                            metrics=['acc']
+                          )
 
     def reset_data_labels(self):
-        """
-        Initializes all the required current data storage devices. 
-        """
         self.training_data, self.training_labels = [], []
         self.testing_data, self.testing_labels = [], []
         self.symbol_to_val = {}
-        self.finished_loading = 0
 
-    def process_symbol_directory(self, data, labels, direc, symbol_names, i):
-        """
-        Helper function for processing all the loading of data symbols and training set data. 
-        """
-        symbol_name = symbol_names[i]
+    def process_symbol_directory(self, data, labels, direc, i):
+        """ Helper function for processing all the loading of data symbols and training set data. """
+        symbol_name = self.symbol_names[i]
         image_files = os.listdir(direc + symbol_name)
         for j in range(len(image_files)):
-            self.append_data(data, labels, direc + symbol_name + '\\' + image_files[j], i)
+            append_data(data, labels, direc + symbol_name + '\\' + image_files[j], i)
             self.symbol_to_val[i] = symbol_name
         self.finished_loading += 1
         if self.finished_loading%10 == 0:
             print("Finished {0} / {1}".format(self.finished_loading, self.num_classes))
 
-    def get_test_from_training(self):
-        random_ints = np.random.randint(len(self.training_labels), size=(int((1-self.TRAINING_TO_TESTING_RATIO)*len(self.training_labels)), 1))
-        random_ints = np.sort(random_ints, 0)
-        for i in range(len(random_ints)):
-            val = random_ints[len(random_ints)-i-1][0]
-            self.testing_data.append(self.training_data[val])
-            self.testing_labels.append(self.training_labels[val])
-            self.training_data.pop(val)
-            self.training_labels.pop(val)
-        print("Finished selecting test data")
+    def process_training_data(self):
+        pool = ThreadPool(self.NUM_THREADS)
+        self.finished_loading = 0
+        args = get_pool_args(
+                            self.training_data, 
+                            self.training_labels, 
+                            self.training_dir, 
+                            self.num_classes
+                            )
+        pool.starmap(self.process_symbol_directory, args)
+        self.training_labels = categorize_labels(self.training_labels, self.num_classes)
+        print("Finished loading training data")
 
-    def categorize_labels(self):
-        """
-        Turn all the current labels into numpy arrays for NN processing. 
-        """
-        self.training_labels = np.reshape(np.array(self.training_labels), (len(self.training_labels), 1))
-        self.training_labels = to_categorical(self.training_labels, num_classes=self.num_classes)
-        self.testing_labels = np.reshape(np.array(self.testing_labels), (len(self.testing_labels), 1))
-        self.testing_labels = to_categorical(self.testing_labels, num_classes=self.num_classes)
-
-    def get_pool_args(self, symbol_names):
-        """
-        Returns an array of the set of distrubuted data processing for Thread pooling.
-        This is the function for no test directory.
-        """
-        args = []
-        for i in range(self.num_classes):
-            args.append(tuple([self.training_data, self.training_labels, self.train_dir, symbol_names, i]))
-        return args
+    def process_testing_data(self):
+        pool = ThreadPool(self.NUM_THREADS)
+        if self.testing_dir:
+            self.finished_loading = 0
+            args = get_pool_args(
+                                self.testing_data, 
+                                self.testing_labels, 
+                                self.testing_dir, 
+                                self.num_classes
+                                )
+            pool.starmap(self.process_symbol_directory, args)
+            self.testing_labels = categorize_labels(self.testing_labels, self.num_classes)
+            print("Finished loading testing data")
 
     def process_data(self):
-        """
-        This function takes the directories specified in the constructor and turns them into tf.datasets
-
-        If the data set given only has one set of data, use 95% for training and the other 5% for testing/validation.
-        If the data set only has one set of data, training is AUTOMATICALLY the first 95% of the data
-        """
+        """ Takes training/testing data directories and turns them into tf.datasets """
         self.reset_data_labels()
-        symbol_names = os.listdir(self.train_dir)
-        self.num_classes = len(symbol_names)
-        
-        self.NUM_THREADS = 50
-        start_50 = time.time()
-
-        pool = ThreadPool(self.NUM_THREADS)
-        args = self.get_pool_args(symbol_names)
-        pool.starmap(self.process_symbol_directory, args)
-        total_50 = (time.time() - start_50)*pow(10, 3) # time in ms
-        self.NUM_THREADS = 100
-        start_100 = time.time()
-        pool = ThreadPool(self.NUM_THREADS)
-        args = self.get_pool_args(symbol_names)
-        pool.starmap(self.process_symbol_directory, args)
-        total_100 = (time.time() - start_50)*pow(10, 3) # time in ms
-
-        print("Finished loading data")
-        if not self.test_dir:
-            self.get_test_from_training()
-        else:
-            testing_data, testing_labels = [], []
-            # data = os.listdir(self.test_dir)
-            # for file in data:
-            #     testing_data.append(np.array(cv2.imread(self.test_dir + symbol_name + '\\' + image_files[i])))
-            #     testing_labels.append(ord(symbol_name))
-        self.categorize_labels()
-        print("Finished process data")
-
+        self.symbol_names = os.listdir(self.training_dir)
+        self.num_classes = len(self.symbol_names)
+        self.process_training_data()
+        self.process_testing_data()
+    # TODO!!!!!
     def get_layers(self):
-        """
-        Set the layers of the CNN. 
-        """
+        """ Set the layers of the CNN. """
         FEATURES, KERNEL = [1, 32, 64, 128, 256], [6, 5, 4, 3, 3]
         LAYERS = len(KERNEL)
 
@@ -145,23 +108,24 @@ class Network:
         self.model.add(Dropout(0.1))
         self.model.add(Dense(self.num_classes, activation='softmax'))
 
-    def create_model(self):
-        self.model = Sequential()
-        self.get_layers()
-        self.model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-    
-    def train_model(self, epochs = 10, batch_size = 128):
-        """
-        Call the model training function. 
-        """
-        self.model.fit(np.array(self.training_data), np.array(self.training_labels), epochs=epochs, 
-                        batch_sizse=batch_size, shuffle=True)
-        self.evaluate_model()
+    def train_model(self, epochs, batch_size):
+        """ Will automatically evaluate model on testing data. """
+        args = tuple(
+                    np.array(self.training_data), 
+                    np.array(self.training_labels)
+                    )
+        kwargs = {
+                    'epochs' : epochs, 
+                    'batch_sizse' : batch_size, 
+                    'shuffle' : True
+                 }
+        if self.testing_data:
+            kwargs['validation_data'] = tuple(self.testing_data, self.testing_labels)
+        else:
+            kwargs['validation_split'] = self.TESTING_TO_TRAINING_RATIO
+        self.model.fit(*args, **kwargs)
 
-    def evaluate_model(self, test_data = None, test_labels = None):
-        past_best_model = load_model(self.save_dir + 'model')
+    def evaluate_model(self):
         best_loss, best_acc = past_best_model.evaluate(np.array(self.testing_data), np.array(self.testing_labels))
         curr_loss, curr_acc = self.model.evaluate(np.array(self.testing_data), np.array(self.testing_labels))
         print("Current loss: " + str(curr_loss), "Current accuracy: " + str(curr_acc))
@@ -169,42 +133,14 @@ class Network:
             print("Best run yet!")
             save_model(self.model, self.save_dir + 'model', overwrite=True, include_optimizer=True)
             best_loss, best_acc = curr_loss, curr_acc
-        
 
-    def implement(self):
-        print("Cnn success")
-
-def main(TRAIN_DIR, 
-         SAVE_DIR, 
-         TEST_DIR, 
-         train = False,
-         implementing = False, 
-         controller = None,   
-         epochs = 10, 
-         batch_size = 1024, 
-         testing_data = None, 
-         testing_labels = None):
-    """
-    """
-    my_network = Network(TRAIN_DIR, SAVE_DIR, TEST_DIR)
+def main():
+    # Default training and testing data directories:
+    TRAIN_DIR, TEST_DIR = 'C:\\Users\\bri25\\Documents\\Python\\data\\', None
+    MODEL_DIR = 'data/'
+    my_network = Network(TRAIN_DIR, MODEL_DIR, TEST_DIR)
     my_network.process_data()
     my_network.create_model()
-    if train:
-        my_network.train_model(epochs = epochs, batch_size = batch_size)
-    elif implementing:
-        # my_network.load_model()
-        my_network.evaluate_model(test_data, test_labels)
-        # controller.set_CNN(my_network)
-    else:
-        # my_network.evaluate_model(epochs, batch_size, testing_data, testing_labels)
-        pass
-
+    
 if __name__ == '__main__':
-    test_data_dir = 'C:\\Users\\bri25\\Desktop\\'
-    filenames = os.listdir(test_data_dir)
-    test_data, test_labels = [], []
-    for fname in filenames:
-        test_data.append(cv2.imread(test_data_dir + fname))
-        test_labels.append('sum')
-    main(TRAIN_DIR, SAVE_DIR, TEST_DIR, implementing = True, testing_data=test_data, testing_labels=test_labels)
-    # main(TRAIN_DIR, SAVE_DIR, TEST_DIR, train=True)
+    main()
---
+++ recognition_lib.py
@@ -0,0 +1,90 @@
+""" Additional functions so recognition.py is a bit cleaner and more straightforward. """
+
+# Library imports
+import numpy as np, cv2
+from tensorflow.python.keras.utils import to_categorical
+
+def check_folder_style(folder_name):
+        """
+        If a folder directory path does not have \\ at the end, add them in.
+        This ensure no problems with file/directory loading in the future. 
+
+        >>> s = check_folder_style("bob")
+        >>> s
+        'bob\\\\'
+        >>> s = check_folder_style("")
+        Traceback (most recent call last):
+            ...
+        AssertionError: Folder directory name cannot be empty
+        >>> s = check_folder_style('bob\\\\')
+        >>> s
+        'bob\\\\'
+        """
+        assert folder_name, "Folder directory name cannot be empty"
+        if folder_name[-1] == "\\":
+            return folder_name
+        return folder_name + "\\"
+
+def append_data(data, labels, fname, name):
+        """
+        Add "fname" to list "data", and "name" to list "labels". 
+
+        >>> append_data([], [], "fakeimage", "")
+        Traceback (most recent call last):
+            ...
+        AssertionError: No image found at: fakeimage
+        >>> import os, numpy as np, cv2
+        >>> images_dir = 'images\\\\'
+        >>> data, labels, images = [], [], os.listdir(images_dir)
+        >>> append_data(data, labels, images_dir + images[0], 'bob')
+        >>> len(data)
+        1
+        >>> labels[0]
+        'bob'
+        """
+        image = cv2.imread(fname)
+        assert image is not None, "No image found at: " + fname
+        data.append(np.array(image))
+        labels.append(name)
+
+def categorize_labels(labels, num_classes):
+        """
+        Turn all the current labels into numpy arrays for NN processing. 
+
+        >>> categorize_labels([], 5)
+        Traceback (most recent call last):
+            ...
+        AssertionError: Labels to categorize cannot be empty
+        >>> categorize_labels([1], 0)
+        Traceback (most recent call last):
+            ...
+        AssertionError: The number of categorization classes must be greater than 0
+        >>> categorize_labels([0, 1, 2, 1], 3)
+        array([[1., 0., 0.],
+               [0., 1., 0.],
+               [0., 0., 1.],
+               [0., 1., 0.]], dtype=float32)
+        """
+        assert len(labels), "Labels to categorize cannot be empty"
+        assert num_classes>0, "The number of categorization classes must be greater than 0"
+        labels = np.reshape(np.array(labels), (len(labels), 1))
+        return to_categorical(labels, num_classes=num_classes)
+
+def add_then_remove(from_arr, to_arr, index):
+    """
+    Removes the value at index from from_arr and appends it to the end of to_arr 
+
+    >>> from_arr, to_arr = [0, 1, 2, 3], []
+    >>> add_then_remove(from_arr, to_arr, 2) 
+    >>> from_arr
+    [0, 1, 3]
+    >>> to_arr
+    [2]
+    """
+    to_arr.append(from_arr[index])
+    from_arr.pop(index)
+
+def get_pool_args(data, labels, from_dir, num_classes):
+        """ Returns an array of the set of distrubuted data processing for Thread pooling. """
+        return [tuple([data, labels, from_dir, i]) for i in range(num_classes)]
+
